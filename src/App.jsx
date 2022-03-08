11c64f597b98ca41e3b6a63c264ff39465bfaeb2
Omri Steinberg <omristg@gmail.com>
2022-03-08 21:03:28 +0200

    header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Route } from "react-router-dom"
+
+import { Navbar } from "./cmps/Navbar"
+
 export const App = () => {
 	return (
-		<>
-			<h1 className="text-3xl font-bold underline">
-				Hello world!
-			</h1>
-			<button className="btn">button</button>
-		</>
+		<Router>
+			<div className="flex flex-col justify-between h-screen">
+				<Navbar>
+					<main>Content</main>
+				</Navbar>
+				</div>
+		</Router>
 	)
 }
