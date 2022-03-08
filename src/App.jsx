8d3880394844d6ef0b920a97d24cf00cb989241a
Omri Steinberg <omristg@gmail.com>
2022-03-08 22:21:29 +0200

    basic routing, about and not found pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import { BrowserRouter as Router, Route } from "react-router-dom"
-import { Footer } from "./cmps/Footer"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
+import { Footer } from "./cmps/Footer"
 import { Navbar } from "./cmps/Navbar"
+import { Home } from "./pages/Home"
+import { NotFound } from "./pages/NotFound"
+import { About } from "./pages/About"
 
 export const App = () => {
 	return (
 		<Router>
 			<div className="flex flex-col justify-between h-screen">
 				<Navbar />
-				<main>Content</main>
+				<main className="container mx-auto px-3 pb-12">
+					<Routes>
+						<Route path='/' element={<Home />} />
+						<Route path='/about' element={<About />} />
+						{/* <Route path='/notfound' element={<NotFound />} /> */}
+						<Route path='*' element={<NotFound />} />
+					</Routes>
+				</main>
 				<Footer />
 			</div>
 		</Router>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export const Home = () => {
+
+    return (
+        <div className="">
+            <h1 className="text-6xl">Welcome</h1>
+        </div>
+    )
+}
