62c00a3761974cbf410e00cad7fe970b0d131981
Omri Steinberg <omristg@gmail.com>
2022-04-26 19:27:11 +0300

    token works

--- src/cmps/users/UserList.jsx
+++ src/cmps/users/UserList.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useContext } from "react"
+import { useContext } from "react"
 import { GithubContext } from "../../context/github/GithubContext"
 
 import { Spinner } from "../layout/Spinner"
